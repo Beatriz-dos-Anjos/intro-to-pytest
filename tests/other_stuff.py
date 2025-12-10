73a365835782840cad71573474c50c1bb78ad505
Beatriz-dos-Anjos <biaanjos2005@gmail.com>
2025-12-10 02:06:36 -0300

    chore(flaky): section completeed

--- tests/other_stuff.py
+++ tests/other_stuff.py
@@ -3,5 +3,4 @@ def test_in_non_test_module():
     PyTest will recognize this function as a test...
     But will not collect tests from this file (by default)
     """
-    
     print("\nRunning test_in_non_test_module...")
